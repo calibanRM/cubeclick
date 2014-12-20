4bbe0d7286185efb52401f3227e5937b22c4634a
tomas <cunn.tomas@gmail.com>
2014-12-20 20:10:42 -0300

    subcontainers y nivel funcionando

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).on("ready",function(){
-	$(".pieza").on("click",function(){
+	$(document).on("click",".pieza",function(){
 		console.log($(this).css("background-color"));
 		if($(this).prop("color")!="red"){
 			$(this).css("background-color","red");
 			$(this).prop("color","red");
 		}else{
 			newSubContainer($(this));
-			$(this).css("background-color","white");
+			$(this).css("background-color","");
 			$(this).prop("color","white");	
 			
 		}
@@ -18,10 +18,21 @@ $(document).on("ready",function(){
 	*====================================
 	*/
 	function newSubContainer(pieza){
-		//remuevo la clase pieza y agrego la clase sub_container
-		pieza.removeClass("pieza").addClass("sub_container");
-		//con esta funcion agrego 4 div dentro de la pieza que recien hice click
-		pieza.html("<div class='pieza'></div><div class='pieza'></div><div class='pieza'></div><div class='pieza'></div>");
+		console.log(pieza.attr("nivel"));
+		var nivel;
+		if(pieza.attr("nivel") == undefined){
+			nivel = 1;
+		}else{
+			nivel = parseInt(pieza.attr("nivel")) + 1;
+		}
+		if(nivel < 5){
+			//remuevo la clase pieza y agrego la clase sub_container
+			pieza.removeClass("pieza").addClass("sub_container");
+			//con esta funcion agrego 4 div dentro de la pieza que recien hice click
+			var div = "<div class='pieza' nivel='"+nivel+"'></div>";
+			pieza.html(div+div+div+div);
+			
+		}
 		
 	}
 });
