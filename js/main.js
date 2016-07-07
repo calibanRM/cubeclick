1fa4a3e5681cbc477b82ffafc23b26755fd81898
ivanhuay <ivanhuay@gmai.com>
2016-07-06 22:32:32 -0300

    auto color added, now is posible play

--- js/main.js
+++ js/main.js
@@ -4,14 +4,11 @@ $(document).on("ready", function() {
      *CAMBIO PARA TRABAJAR CON CLASE ROJO ===
      *=======================================
      */
-    //DOCUMENTACION DE LAS FUNCIONES USADAS
-    //http://api.jquery.com/hasclass/
-    //http://api.jquery.com/addClass/
-    //http://api.jquery.com/removeClass/
     $(document).on("click", ".pieza", function() {
         console.log($(this).css("background-color"));
         if (!$(this).hasClass("rojo")) {
-            $(this).addClass("rojo");
+            //deberia bajarle puntos
+            // $(this).addClass("rojo");
         } else {
             newSubContainer($(this));
             $(this).removeClass("rojo");
@@ -23,46 +20,61 @@ $(document).on("ready", function() {
      *FUNCION PARA AGREGAR SUBCONTAINER===
      *====================================
      */
-    function newSubContainer(pieza) {
-        console.log(pieza.attr("nivel"));
-        var nivel;
-        if (pieza.attr("nivel") === undefined) {
-            nivel = 1;
+    function newSubContainer(box) {
+        var leven = 0;
+        if (box.attr("nivel") === undefined) {
+            leven = 1;
         } else {
-            nivel = parseInt(pieza.attr("nivel")) + 1;
+            leven = parseInt(box.attr("nivel")) + 1;
         }
-        if (nivel < 5) {
-            //remuevo la clase pieza y agrego la clase sub_container
-            pieza.removeClass("pieza").addClass("sub_container");
-            //con esta funcion agrego 4 div dentro de la pieza que recien hice click
-            var div = "<div class='pieza' nivel='" + nivel + "'></div>";
-            pieza.html(div + div + div + div);
-
+        if (leven < 5) {
+            box.removeClass("pieza").addClass("sub_container");
+            var div = "<div class='pieza' nivel='" + leven + "'></div>";
+            box.html(div + div + div + div);
+        } else {
+            box.addClass("removed");
         }
+    }
+    /*
+     *===================================
+     *FUNCTION FOR ADDCLASS RANDOM ======
+     *===================================
+     */
+    //es mejor usar una funcion mas generica que simplemente colorear los div
+    var randomAddClassObjects = function(elements, objClass) {
+        var midLenght = Math.floor(elements.length / 2); //tomo como maximo la mitad redondeada hacia abajo
+        elements.removeClass(objClass); //saco la clase a todos
 
+        var index = null; //solo instancio la variable(es una buena practica y es mas legible)
+        for (var i = 0; i < midLenght; i++) {
+            index = Math.round(Math.random() * elements.length); //agarro un random sin preocupare por q se repitan ni nada
+            $(elements[index]).addClass(objClass);
+        }
+    };
+
+    var minuto = 2;
+    var segundo = 0;
+    $("#reloj").prop("disabled", true);
+    $(document).on("click", "#start", function() {
+        crono();
+    });
+
+    function crono() {
+        randomAddClassObjects($(".pieza"), "rojo");
+        $("#reloj").val(minuto + " : " + segundo);
+        var timeout = setTimeout(crono, 1000);
+        segundo = segundo - 1;
+        if (segundo <= -1 && minuto <= 0) {
+            clearTimeout(timeout);
+        }
+        if (segundo < 0) {
+            minuto = minuto - 1;
+            segundo = 59;
+        }
+        bloqueoBtn();
     }
 
-        var minuto = 2;
-        var segundo = 0;
-        $("#reloj").prop("disabled",true);
-    $(document).on("click","#start", function(){  crono();  })
-        function crono(){
-            $("#reloj").val(minuto + " : " + segundo);
-            var timeout = setTimeout(crono,1000);
-            
-            segundo = segundo-1;
-            if(segundo <= -1 && minuto <= 0){
-                clearTimeout(timeout);
-                
-            }
-            if(segundo < 0){
-                minuto = minuto-1;
-                segundo = 59;
-            }
-        bloqueoBtn();
-        }
-        
-        function bloqueoBtn(){
-            $("#start").prop("disabled",true);
-        }     
-    });
+    function bloqueoBtn() {
+        $("#start").prop("disabled", true);
+    }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Game for testing
+
+todo: el codigo esta en español, para tener mas universalidad conviene pasarlo a ingles.
